892ff2fefc39ec8a3241d361212123b48a17eadc
giovanna <giovannasilveira0506@gmail.com>
2023-08-22 16:15:02 -0300

    giogi

---
+++ meumodulo.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var dt = require('./meumodulo');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
